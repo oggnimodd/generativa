e68650f30dcf815271ad270539310d249d9cea48
dominggo1999 <arnolddominggo19@gmail.com>
2022-03-21 21:21:37 +0700

    refresh button shadow

--- src/common/RefreshButton/RefreshButton.style.jsx
+++ src/common/RefreshButton/RefreshButton.style.jsx
@@ -15,10 +15,12 @@ export const RefreshToolTip = styled.div`
     mr-2
     w-24
     rounded-sm 
-    outline-none
+    outline-none 
+    pointer-events-none
   `}
 
   transition : opacity ease-in-out 250ms;
+  box-shadow: rgba(1, 1, 7, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.712) 0px 8px 16px -8px;
 `;
 
 export const RefreshButtonWrapper = styled.button`
@@ -31,6 +33,9 @@ export const RefreshButtonWrapper = styled.button`
     h-[55px] 
     bg-accent 
   `}
+
+  box-shadow: rgba(1, 1, 7, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.712) 0px 8px 16px -8px;
+
 
   &:hover svg{
     transform : rotate(360deg) scale(1.2);
