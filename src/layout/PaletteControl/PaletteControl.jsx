c1dac04640a0fa4583f30029f455d4a3d03dadb9
dominggo1999 <arnolddominggo19@gmail.com>
2022-03-25 14:24:56 +0700

    add more palette

--- src/layout/PaletteControl/PaletteControl.jsx
+++ src/layout/PaletteControl/PaletteControl.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { ControlHeader } from '../../common/ControlHeader';
 import PalettePicker from '../../common/PalettePicker/PalettePicker';
 import RandomPaletteButton from '../../common/RandomPaletteButton/RandomPaletteButton';
+import useSettingsStore from '../../store/useSettingsStore';
+
+const showColor = () => {
+  console.log(useSettingsStore.getState().settings.activePalette);
+};
 
 const PaletteControl = () => {
   return (
@@ -9,6 +14,7 @@ const PaletteControl = () => {
       <ControlHeader>Choose Palette</ControlHeader>
       <PalettePicker />
       <RandomPaletteButton />
+      <button onClick={showColor}>Show color</button>
     </>
   );
 };
