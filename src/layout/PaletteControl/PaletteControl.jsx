cfc502a56e95fb12bb1c6ca7ee3436ac04c54319
dominggo1999 <arnolddominggo19@gmail.com>
2022-03-25 14:43:15 +0700

    remove 'show color' button

--- src/layout/PaletteControl/PaletteControl.jsx
+++ src/layout/PaletteControl/PaletteControl.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { ControlHeader } from '../../common/ControlHeader';
 import PalettePicker from '../../common/PalettePicker/PalettePicker';
 import RandomPaletteButton from '../../common/RandomPaletteButton/RandomPaletteButton';
-import useSettingsStore from '../../store/useSettingsStore';
-
-const showColor = () => {
-  console.log(useSettingsStore.getState().settings.activePalette);
-};
 
 const PaletteControl = () => {
   return (
@@ -14,7 +9,6 @@ const PaletteControl = () => {
       <ControlHeader>Choose Palette</ControlHeader>
       <PalettePicker />
       <RandomPaletteButton />
-      <button onClick={showColor}>Show color</button>
     </>
   );
 };
